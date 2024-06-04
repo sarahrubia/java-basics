7cdea8dcbc92c13ff7046d23de93468e4a8a0fe9
sarahrubia <sarahrubia22@gmail.com>
2024-06-04 17:57:59 -0300

    feat: create new project MyClass

---
+++ ClassAnatomy/src/MyClass.java
@@ -0,0 +1,7 @@
+public class MyClass {
+  // recomenda-se que apenas uma classe tenha o método main em uma aplicação
+  public static void main (String [] args) {
+    // main é sempre public, static, não tem retorno (void) e recebe um array de strings
+    System.out.print("my class");    
+  }
+}
